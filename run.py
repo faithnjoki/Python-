75e6ed639e37d976da6044ac3db476f77fb3dfc3
faithnjoki <faithnjoki81@gmail.com>
2021-11-09 11:02:46 +0300

    Initialize repository

---
+++ run.py
@@ -0,0 +1,18 @@
+# first py 
+# fname =  ' leon'
+
+# print ('My name is ' + fname)
+# print('My name is {}'.format(fname))
+# print(f'My name is {fname}')
+
+
+
+noun1 = input('noun')
+noun2 = input('noun')
+adj = input('adjective')
+movie = input('movie')
+
+
+madlib = f'My name is faith {noun1}. my son is called Leon {noun2}!\ I love {adj} and like watching {movie}' 
+print(madlib)
+
