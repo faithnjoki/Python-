fba728748d21f8eaede44116665ad1c4542ff74a
faithnjoki <faithnjoki81@gmail.com>
2021-11-10 18:55:02 +0300

    start number gueser py

---
+++ Numbergueser.py
@@ -0,0 +1,7 @@
+# import random module
+import random
+
+# random.randrange(start,stop)
+# random.randrange(6,8), This generates random numbers between 6 and 8 but wont generate 8. Incase you wate to generate 8 you have to add 9 as the end point
+
+
